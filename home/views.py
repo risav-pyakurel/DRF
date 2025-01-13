673fb644a8b0b84c80bbd0bf0a130898f83ee866
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-13 12:01:49 +0545

    learning api views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def index(request):
+    courses= {
+        'course_name': 'python',
+        'learn': {'flask', 'django', 'Tornado', 'fastapi'},
+        'course_provider' : 'risav'
+    }
+    return Response(courses)
