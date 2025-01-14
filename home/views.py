b147b491fed866e6e0fc629b69244339b0049bcb
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-14 22:36:37 +0545

    new model addition

--- home/views.py
+++ home/views.py
@@ -4,8 +4,18 @@ from rest_framework.response import Response
 @api_view(['GET'])
 def index(request):
     courses= {
-        'course_name': 'python',
-        'learn': {'flask', 'django', 'Tornado', 'fastapi'},
-        'course_provider' : 'risav'
-    }
-    return Response(courses)
+            'course_name': 'python',
+            'learn': {'flask', 'django', 'Tornado', 'fastapi'},
+            'course_provider' : 'risav'
+        }
+    
+    if request.method == 'GET':
+        print("it's a get response" )
+        return Response(courses)
+    elif request.method == 'POST':
+        print("you hit a post method")
+        return Response()
+        
+
+    
+    
