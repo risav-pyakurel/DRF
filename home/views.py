87183d9118e1dfcbee9e24e75ccff31bd0eec8b8
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-18 22:56:29 +0545

    request data

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
+
+ 
 @api_view(['GET'])
 def index(request):
     courses= {
@@ -13,7 +16,14 @@ def index(request):
         print("it's a get response" )
         return Response(courses)
     elif request.method == 'POST':
+        data = request.data
+        print("##############")
+        print(data)
         print("you hit a post method")
+        return Response()
+    
+    elif request.method == 'PUT':
+        print("you hit a put method")
         return Response()
         
 
