b3f94ee0d9d374b1db7799e034fe74ce32bbeab1
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-18 22:57:20 +0545

    new changes

--- home/serializers.py
+++ home/serializers.py
@@ -1,6 +1,2 @@
 from rest_framework import serializers
 
-class CommentSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    content = serializers.CharField(max_length=200)
-    created = serializers.DateTimeField()
