7323da103c29802b02d4a2c651eb3f347b4322d6
risav-pyakurel <pyakurel.risav@gmail.com>
2025-01-18 23:01:40 +0545

    new serializer

--- home/serializers.py
+++ home/serializers.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
+from .models import Person
 
+class PeopleSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Person
+        fields = ['name','age'] # if we want to serialize all the fields we can also type '__all__'
+        #exclude = [''] helps to exclude the fields we don't want to exclude
